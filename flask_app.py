dc8faabcce0dc05210885ce7e036d2056bd51fdd
Karan1 <karansj4723@gmail.com>
2025-03-29 11:33:21 +0530

    i have added home page

--- flask_app.py
+++ flask_app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/page2")
+def home1():
+    return render_template('home.html')
+
 # Default port:
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
